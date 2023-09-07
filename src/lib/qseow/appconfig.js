e56f389acef3cf0d4065cb7de4a54485b465e526
Göran Sander <goran.sander@gmail.com>
2023-09-07 09:12:22 +0200

    feat: Add support for comments in cloud app ID definition editor Implements #38

--- src/lib/qseow/appconfig.js
+++ src/lib/qseow/appconfig.js
@@ -4,30 +4,7 @@ function getCandidateAppsPredefAndIncoming(node, done, msg) {
     let appIdCandidates = [];
 
     // Where should we get the app identifiers from?
-    if (node.appSource1 === 'predefined') {
-        // Use the app Ids in the node configuration
-        // Break the \n separated list of app ids into an array
-        let appIdsPredefined = node.appId.split('\n');
-
-        // Remove any empty elements from the arrayy
-        appIdsPredefined = appIdsPredefined.filter((el) => el !== '');
-
-        // Remove any rows that have # (comment) as first non-whitespace or space character
-        appIdsPredefined = appIdsPredefined.filter((el) => !el.match(/^\s*#/));
-
-        // Use the app ids in the node configuration
-        // Log the app ids
-        node.log(`Predefined app ids: ${appIdsPredefined}`);
-
-        // Were there any app ids specified?
-        if (appIdsPredefined.length === 0) {
-            appIdCandidates = [];
-        } else {
-            appIdsPredefined.forEach((appId) => {
-                appIdCandidates.push(appId);
-            });
-        }
-    } else if (node.appSource1 === 'msg-in') {
+    if (node.appSource1 === 'msg-in') {
         let appIdsIncoming;
 
         // Use the app ids in the incoming message
@@ -53,6 +30,29 @@ function getCandidateAppsPredefAndIncoming(node, done, msg) {
             appIdCandidates = [];
         } else {
             appIdsIncoming.forEach((appId) => {
+                appIdCandidates.push(appId);
+            });
+        }
+    } else if (node.appSource1 === 'predefined') {
+        // Use the app Ids in the node configuration
+        // Break the \n separated list of app ids into an array
+        let appIdsPredefined = node.appId.split('\n');
+
+        // Remove any empty elements from the arrayy
+        appIdsPredefined = appIdsPredefined.filter((el) => el !== '');
+
+        // Remove any rows that have # (comment) as first non-whitespace or space character
+        appIdsPredefined = appIdsPredefined.filter((el) => !el.match(/^\s*#/));
+
+        // Use the app ids in the node configuration
+        // Log the app ids
+        node.log(`Predefined app ids: ${appIdsPredefined}`);
+
+        // Were there any app ids specified?
+        if (appIdsPredefined.length === 0) {
+            appIdCandidates = [];
+        } else {
+            appIdsPredefined.forEach((appId) => {
                 appIdCandidates.push(appId);
             });
         }
